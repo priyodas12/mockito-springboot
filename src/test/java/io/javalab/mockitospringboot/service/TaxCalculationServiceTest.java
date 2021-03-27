d8407e463e5b10ad2e1abd4416e6dcfbdc6cf0af
Priyobrato Das <priyodas12@gmail.com>
2021-03-27 19:18:25 +0530

    new test class

--- src/test/java/io/javalab/mockitospringboot/service/TaxCalculationServiceTest.java
+++ src/test/java/io/javalab/mockitospringboot/service/TaxCalculationServiceTest.java
@@ -2,6 +2,7 @@ package io.javalab.mockitospringboot.service;
 
 import io.javalab.mockitospringboot.service.external.QuarterGeneratorService;
 import io.javalab.mockitospringboot.service.external.QuarterGeneratorServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -13,15 +14,27 @@ import static org.mockito.Mockito.when;
 
 class TaxCalculationServiceTest {
 
-    @Test
-    void calculateTaxPerQuarter() throws Exception {
 
-        QuarterGeneratorService service=mock(QuarterGeneratorServiceImpl.class);
-        TaxCalculation taxCalculation=new TaxCalculation();
+    TaxCalculation taxCalculation=new TaxCalculation();
+    QuarterGeneratorService service=mock(QuarterGeneratorService.class);
+
+    @BeforeEach
+    public void beforeAll(){
         taxCalculation.setQuarterGeneratorService(service);
+    }
+
+    @Test
+    void calculateTaxPerQuarter_basic() throws Exception {
         when(taxCalculation.calculateTaxPerQuarter(1)).thenReturn(new Integer[]{4,5,6});
         Optional<Integer> actualResult= Arrays.stream(taxCalculation.calculateTaxPerQuarter(1)).reduce(Integer::sum);
         int expectedResult=15;
+        assertEquals(expectedResult,actualResult.get());
+    }
+    @Test
+    void calculateTaxPerQuarter_empty() throws Exception {
+        when(taxCalculation.calculateTaxPerQuarter(9)).thenReturn(new Integer[]{});
+        Optional<Integer> actualResult= Optional.of(Arrays.stream(taxCalculation.calculateTaxPerQuarter(9)).reduce(Integer::sum).orElse(0));
+        int expectedResult=0;
         assertEquals(expectedResult,actualResult.get());
     }
 }
